5ca5766cef41f037ed29b07bede076cea88bd4c5
Keru Wang <kw2727@nyu.edu>
2022-03-03 09:59:21 -0500

    changed from worldLandmarks to landmarks for the hand tracking data

--- js/scenes/demoDesktopHand.js
+++ js/scenes/demoDesktopHand.js
@@ -5,20 +5,20 @@ export const init = async model => {
     let jointSize = [[],[]];
     for(let i = 0; i < 2; i ++)
         for(let j = 0; j < jointNum; j ++) {
-            joint[i][j] = {mesh: model.add('sphere'), detected: false};
+            joint[i][j] = {mesh: model.add('sphere').color(1,0,0), detected: false};
         }
             
     model.animate(() => {
 
         for(let i = 0; i < window.handInfo.length; i ++) {
             for(let j = 0; j < jointNum; j ++) {
-                joint[i][j].mesh.identity().move(window.handInfo[i].worldLandmarks[j].x,2 - window.handInfo[i].worldLandmarks[j].y,window.handInfo[i].worldLandmarks[j].z);
+                joint[i][j].mesh.identity().move(-0.5 + window.handInfo[i].landmarks[j].x,2 - window.handInfo[i].landmarks[j].y,window.handInfo[i].landmarks[j].z);
                 joint[i][j]. detected = true;
             } 
         }
 
         for(let i = 0; i < 2; i ++)
             for(let j = 0; j < jointNum; j ++) 
-                joint[i][j].mesh.scale(joint[i][j].detected ? 0.01 : 0);
+                joint[i][j].mesh.scale(joint[i][j].detected ? 0.02 : 0);
     });
  }
